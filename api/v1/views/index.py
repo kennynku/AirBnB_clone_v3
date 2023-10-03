c31e607ec82c62bca1071ba11533c0520f427a5f
Ntsako Ngoveni <naction723@gmail.com>
2023-10-03 05:03:46 -0700

    final checks

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,20 +5,22 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
+@app_views.route('/status', methods=['GET'])
+def api_status():
     """ Returns JSON """
-    return jsonify(status="OK")
+    response = ('status': 'OK')
+    return jsonify(response)
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stat():
+@app_views.route('/stats', methods=['GET'])
+def get_stat():
     """Provides the count of objects based on respective types"""
-    return jsonify(
-        amenities=storage.count('Amenity'),
-        cities=storage.count('City'),
-        places=storage.count('Place'),
-        reviews=storage.count('Review'),
-        states=storage.count('State'),
-        users=storage.count('User')
-    )
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+        }
+    return jsonify(stats)
